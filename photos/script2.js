b59c65321947b238d87243ad92f809019227788b
DESKTOP-AL1M89N <kumarbhattarai666@gmail.com>
2025-01-08 22:26:34 +0545

    updates

--- photos/script2.js
+++ photos/script2.js
@@ -1,28 +1,17 @@
-// Alert user to use desktop for a better experience
-// alert("Use desktop for better experience");
 
-// Get references to the container and buttons
 let container = document.getElementById("ab");
 let back = document.getElementById("backbtn");
 let next = document.getElementById("nextbtn");
 
-// Function to check if the user is on a mobile device
 function isMobileDevice() {
   return /Mobi|Android/i.test(navigator.userAgent);
 }
 
-// Adjust styles and behavior for mobile devices
 if (isMobileDevice()) {
-  // Hide next and back buttons
   back.style.display = "none";
   next.style.display = "none";
 
-  // Increase image width for better viewing on mobile
   const images = container.getElementsByTagName("img");
-//   for (let img of images) {
-//     // img.style.width = "100vw";
-//     // img.style.height="40vh" // Make images full width
-//   }
 
   container.style.width = "100%";
   container.style.overflowX = "scroll"; 
--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+function isMobileDevice() {
+    return /Mobi|Android/i.test(navigator.userAgent);
+  }
 function showSidebar() {
     const sidebar = document.querySelector('.sidebar');
 
@@ -45,3 +48,139 @@ function scrollDown() {
         behavior: 'smooth',
     });
 }
+window.addEventListener('DOMContentLoaded', () => {
+    // Check if the page has been loaded before
+    if (!localStorage.getItem('isFirstLoad')) {
+        // Set a timeout to hide the loader after 2 seconds
+        setTimeout(() => {
+            document.getElementById('loader').style.display = 'none'; 
+            document.body.classList.remove('loading'); 
+        }, 2000);
+
+        // Mark that the page has been loaded once by setting a flag in localStorage
+        localStorage.setItem('isFirstLoad', 'true');
+    }
+    else{
+        document.getElementById('loader').style.display = 'none'; 
+        document.body.classList.remove('loading'); 
+    }
+});
+
+
+const about_me = document.querySelectorAll('.me');
+about_me.forEach((el)=>{
+document.addEventListener('scroll', () => {
+    if (isMobileDevice()) {
+        if (about_me && window.scrollY > 100) {
+            el.classList.add("animate__animated", "animate__fadeInDown", "text-center");
+            el.style.visibility="visible";
+        }
+        else{
+            el.classList.remove("animate__animated", "animate__fadeInDown", "text-center");
+            el.style.visibility="hidden";
+        }
+      }
+      else{
+    if (about_me && window.scrollY > 250) {
+        el.classList.add("mt-2", "animate__animated", "animate__fadeInDown", "text-center");
+        el.style.visibility="visible";
+    }
+    else{
+        el.classList.remove("mt-2", "animate__animated", "animate__fadeInDown", "text-center");
+        el.style.visibility="hidden";
+    }
+}
+});
+});
+const left = document.querySelector('.left');
+document.addEventListener('scroll', () => {
+    if (isMobileDevice()) {
+        if (left && window.scrollY > 300) {
+            left.classList.add("animate__animated", "animate__fadeInUp", "text-center");
+            left.style.visibility="visible";
+        }
+        else{
+            left.classList.remove("animate__animated", "animate__fadeInUp", "text-center");
+            left.style.visibility="hidden";
+        }
+      }
+      else{
+            if (left && window.scrollY > 400) {
+                left.classList.add("animate__animated", "animate__fadeInUp", "text-center");
+                left.style.visibility="visible";
+    }
+            else{
+                left.classList.remove("animate__animated", "animate__fadeInUp", "text-center");
+                left.style.visibility="hidden";
+    }
+}
+});
+const right = document.querySelector('.right');
+document.addEventListener('scroll', () => {
+    if (isMobileDevice()) {
+        if (right && window.scrollY > 750) {
+            right.classList.add("animate__animated", "animate__fadeInUp", "text-center");
+            right.style.visibility="visible";
+        }
+        else{
+            right.classList.remove("animate__animated", "animate__fadeInUp", "text-center");
+            right.style.visibility="hidden";
+        }
+      }
+      else{
+            if (right && window.scrollY > 400) {
+                right.classList.add("animate__animated", "animate__fadeInUp", "text-center");
+                right.style.visibility="visible";
+    }
+            else{
+                right.classList.remove("animate__animated", "animate__fadeInUp", "text-center");
+                right.style.visibility="hidden";
+    }
+}
+});
+const mid = document.querySelector('.mid');
+document.addEventListener('scroll', () => {
+    if (isMobileDevice()) {
+        if (mid && window.scrollY > 500) {
+            mid.classList.add("animate__animated", "animate__fadeInUp", "text-center");
+            mid.style.visibility="visible";
+        }
+        else{
+            mid.classList.remove("animate__animated", "animate__fadeInUp", "text-center");
+            mid.style.visibility="hidden";
+        }
+      }else{
+    if (mid && window.scrollY > 400) {
+        mid.classList.add("animate__animated", "animate__fadeInUp", "text-center");
+        mid.style.visibility="visible";
+    }
+    else{
+        mid.classList.remove("animate__animated", "animate__fadeInUp", "text-center");
+        mid.style.visibility="hidden";
+    }
+}
+});
+const footer = document.querySelector('.footer');
+document.addEventListener('scroll', () => {
+    if (isMobileDevice()) {
+        if (footer && window.scrollY >1750) {
+            footer.classList.add("animate__animated", "animate__fadeInUp", "text-center");
+            footer.style.visibility="visible";
+        }
+        else{
+            footer.classList.remove("animate__animated", "animate__fadeInUp", "text-center");
+            footer.style.visibility="hidden";
+        }
+      }else{
+    if (footer && window.scrollY > 1250) {
+        footer.classList.add("animate__animated", "animate__fadeInUp", "text-center");
+        footer.style.visibility="visible";
+    }
+    else{
+        footer.classList.remove("animate__animated", "animate__fadeInUp", "text-center");
+        footer.style.visibility="hidden";
+    }
+}
+});
+
+
